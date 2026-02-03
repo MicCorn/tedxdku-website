8b8855ebcaae183b1c7e6f8303fb6ac1b0c8161b
Michael Cornell <michaelbriancornell@gmail.com>
2026-02-03 23:26:44 +0800

    Update archive

---
+++ Sparks – TEDxDKU_files/coblocks-animation.js
@@ -0,0 +1,32 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+(()=>{const e=document.querySelectorAll(".coblocks-animate");if("IntersectionObserver"in window){const t=new IntersectionObserver((e=>{e.forEach((e=>{e.isIntersecting&&(e.target.classList.add(e.target.dataset.coblocksAnimation),t.unobserve(e.target))}))}),{threshold:[.15]});e.forEach((e=>{t.observe(e)}))}else e.forEach((e=>{e.classList.remove("coblocks-animate"),delete e.dataset.coblocksAnimation}))})();
+}
+/*
+     FILE ARCHIVED ON 14:39:19 Dec 02, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:12:01 Jan 12, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.61
+  exclusion.robots: 0.023
+  exclusion.robots.policy: 0.01
+  esindex: 0.013
+  cdx.remote: 125.767
+  LoadShardBlock: 324.432 (3)
+  PetaboxLoader3.datanode: 464.925 (5)
+  load_resource: 799.181
+  PetaboxLoader3.resolve: 392.861
+  loaddict: 322.007
+*/
